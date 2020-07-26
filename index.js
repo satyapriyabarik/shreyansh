98f73ae372d6fdd499e6dfd3d1956238f00eeb16
satya <satya.barik27@gmail.com>
2020-07-26 14:05:28 +0530

    new repo content added

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Import stylesheets
 import './style.css';
-
+import './css/news.css';
+import './css/custom.css';
+import './css/spacegallery.css';
+import './js/jquerylib.js';
+import './js/eye.js';
+import './js/spacegallery.js';
+import './js/jflip.js';
+import './js/jquery.blockUI.js';
+import './js/layout.js';
+import './js/moving_slider.js';
+import './js/rain.js';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
