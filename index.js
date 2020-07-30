d21388e83e96b61d7949c768785d6ccd263d7426
satya <satya.barik27@gmail.com>
2020-07-30 18:44:22 +0530

    aaaaaaaad

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ import './js/jflip.js';
 import './js/jquery.blockUI.js';
 import './js/layout.js';
 import './js/prefixfree.min.js';
+import './js/hammer.min.js'
+import './js/jquery.hammer.min.js';
 import './js/custom.js';
 //import './js/rain.js';
 // Write Javascript code!
