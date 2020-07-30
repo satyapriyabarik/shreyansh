b5646c0db36d242f8beafd151de499871aafeab9
satya <satya.barik27@gmail.com>
2020-07-30 16:25:39 +0530

    see

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import './style.css';
 import './css/news.css';
 import './css/custom.css';
 import './css/spacegallery.css';
+import './css/bootstrap.min.css';
+import './css/bookblock.css';
+import '.css/demo4.css';
 import './js/jquerylib.js';
 import './js/jquery-1.7.1.min.js'
 import './js/eye.js';
@@ -10,6 +13,9 @@ import './js/spacegallery.js';
 import './js/jflip.js';
 import './js/jquery.blockUI.js';
 import './js/layout.js';
+import './js/jquery.bookblock.js';
+import './js/jquerypp.custom.js';
+import './js/modernizr.custom.js';
 //import './js/moving_slider.js';
 //import './js/rain.js';
 // Write Javascript code!
