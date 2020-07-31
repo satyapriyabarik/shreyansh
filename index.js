524d96604905ee5f1a6f90fe682ac44a20cc55fe
satya <satya.barik27@gmail.com>
2020-08-01 03:25:53 +0530

    454

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ import './css/spacegallery.css';
 import './css/bootstrap.min.css';
 import './css/jquerysctipttop.css';
 //import './js/jquerylib.js';
-import 'require.js'
-var $ = require( "jquery" );
+
 import './js/prefixfree.min.js';
 //import './js/hammer.min.js'
 //mport './js/jquery.hammer.min.js';
